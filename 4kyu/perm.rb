07c83493970750844f78be7e8438cea591d22a54
Daniel5791 <danielmiller5791@gmail.com>
2018-02-21 10:07:58 +0000

    changes done

---
+++ 4kyu/perm.rb
@@ -0,0 +1,34 @@
+=begin
+	
+solution to this kata: https://www.codewars.com/kata/permutations
+
+=end
+
+
+def permutations(str, pos)
+
+
+
+end
+
+
+
+
+
+
+
+
+
+#beginning of main
+
+	puts "enter string"
+	ans = gets
+	ans = ans.split""
+	print ans
+
+	permutations(ans, 0)
+
+
+
+
+#end of main
