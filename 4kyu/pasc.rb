b1a99b0c02e15bb356bf18497c610d2b06828d73
Daniel5791 <danielmiller5791@gmail.com>
2018-02-23 15:31:55 +0000

    completed pascals

--- 4kyu/pasc.rb
+++ 4kyu/pasc.rb
@@ -1,4 +1,8 @@
+=begin
+	
+solution to this kata: https://www.codewars.com/kata/5226eb40316b56c8d500030f/solutions/ruby
 
+=end
 
 
 def getFactorial fact
@@ -6,22 +10,32 @@ def getFactorial fact
 	#iterates through every number, storing the result of the value * operation
 end
 
-
 def	pascalsTriangle(n)
 
 	triangle = []
-
 	count = 1
+	n-=1
+	#pascals triangle indexes at 0
 
-	while n > 0
-		factTree = getFactorial(n)
-		puts "factTree is #{factTree}"
-		count = 1
-		while count < n
+	while n >= 0
+		tree = getFactorial(n)
+		count = 0
+		while count <= n
+			if count == 0 or count == n
+				pos = 1
+			else
+				current = getFactorial(count)
+				neg = getFactorial(n - count)
+				pos = tree / (current * neg)
+				#uses equation inTriangle = Tree! / (Current! * (T - C)!)
+			end
 			count+=1
+			triangle << pos
 		end
 		n-=1
 	end
+
+	triangle.reverse
 
 end
 
@@ -31,6 +45,6 @@ end
 	puts "enter number to view triangle"
 	n = gets.to_i
 
-	pascalsTriangle(n)
+	print pascalsTriangle(n)
 
 #end of main
