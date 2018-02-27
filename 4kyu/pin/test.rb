2ee5f6f8c872d4316d5cd1d250e4def8f06d5db8
Daniel5791 <danielmiller5791@gmail.com>
2018-02-27 11:35:09 +0000

    challenge completed but not working on grader

--- 4kyu/pin/test.rb
+++ 4kyu/pin/test.rb
@@ -1,5 +1,4 @@
-#program not going to work currently, need to update permutation algorithm
-
+#completed to work with code wars
 $PIN = 
 	[ 
 	[nil, nil, nil, nil, nil], 
@@ -9,88 +8,76 @@ $PIN =
 	[nil, nil, 0, nil, nil] 
 ]
 
+def get_pins(observed)
 
-class CrackSafe
+ 	options = {}
+	optsArray = []
+	current = {}
+	count = 0
 
-	#function for getting all relevant number for perms
-	def initialize(strings)
+	observed = observed.split""
 
-		@options = {}
-		@optsArray = []
-		current = {}
-		count = 0
+	observed.each do | num |
 
-		strings.each_char { | num | num = num.to_i
+		num = num.to_i
+		hold = []
+		row = $PIN.detect { | ind | ind.include?(num)}
+		indx = [row.index(num), $PIN.index(row)]
 
-			hold = []
+		row = indx[0]
+		col = indx[1]
 
-			row = $PIN.detect { | ind | ind.include?(num)}
-			#identifies the row of the 2d array with the number
-			indx = [row.index(num), $PIN.index(row)]
-			#identifies the column 
+		hold << num
+		hold << $PIN[col][row + 1]
+		hold << $PIN[col][row - 1]
+		hold << $PIN[col + 1][row]
+		hold << $PIN[col - 1][row]
 
-			row = indx[0]
-			col = indx[1]
+		optsArray << hold
+		hold.delete_if { | i | i == nil }
+		current = {count => hold}
+		count+=1
+		options.merge!(current)
+    
+    end
 
-			hold << num
-			hold << $PIN[col][row + 1]
-			hold << $PIN[col][row - 1]
-			hold << $PIN[col + 1][row]
-			hold << $PIN[col - 1][row]
 
-			@optsArray << hold
-			#holds all the possible combinations
-
-			hold.delete_if { | i | i == nil }
-
-			current = {count => hold}
-			count+=1
-			@options.merge!(current)
-			
-		}
-
-	
-	end
-
-	def getPerms
-
-		@optsArray = @optsArray.join.split""
-		perms = @optsArray.combination(@options.length)
-		#every single permutation of password
-		final  = []
-
-		perms.each { | perm | 
-			if isValid(perm) == true
-				final << perm.join.to_s
-			end
-		}
-
-		final = final.uniq
-		puts "combos are: \n"
-		final.each { | array | print array.to_s + "\n" }
-
-	end
-
-	def isValid(current)
-		for i in 0..current.length-1
-			@options[i] = @options[i].to_a
-			if @options[i].include?(current[i].to_i) == false
-				return false
-			end
-		end
-
-		return true
-	end
+	return getPerms(optsArray, options)
 
 end
 
-#beginning of main
+def getPerms(optsArray, options)
 
-	puts "enter permutation combination"
-	num = gets.chomp
-	perms = CrackSafe.new(num)
-	perms.getPerms
-	
-#end of main
+	optsArray = optsArray.join.split""
+	perms = optsArray.combination(options.length)
+	#every single permutation of password
+	final  = []
 
+	perms.each do | perm |
+		if isValid(perm, options) == true
+			final << perm.join.to_s
+		end
+	end
+
+	return final.uniq
 	
+end
+
+def isValid(current, options)
+	
+	for i in 0..current.length-1
+		options[i] = options[i].to_a
+		if options[i].include?(current[i].to_i) == false
+			return false
+		end
+	end
+
+	return true
+
+end
+
+
+puts "enter password"
+pass = gets.chomp
+puts get_pins(pass)
+
